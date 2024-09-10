67151d430c7f1864d93d70b9355b711b590cee14
sbeczkiewicz <sbeczkiewicz32@gmail.com>
2024-09-10 11:06:24 -0500

    feat: add a seperate component for form after submit

---
+++ src/components/SubmittedDisplay.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { computed } from 'vue'
+import { useFormStore } from '@/stores/formStore'
+
+
+// Use central state to display submitted data
+const formStore = useFormStore()
+const formData = computed(() => formStore.formData)
+
+//Convert camelCase key to nicer format
+function formatCamelCase(str) {
+  return str.replace(/([a-z])([A-Z])/g, '$1 $2')
+            .replace(/^./, str[0].toUpperCase());
+}
+
+</script>
+
+<template>
+    <div class="display-container">
+      <div v-for="(value, key) in formData" :key="key" class="display-item">
+        <div class="item-key">{{ formatCamelCase(key) }}:</div>
+        <div class="item-value">{{ value }}</div>
+        </div>
+    </div>
+</template>
+
+<style>
+.display-container {
+  display: grid;
+  float: left;
+  gap: 12px;
+  background-color: #f9f9f9;
+  padding: 16px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  width: 400px;
+  margin: 16px auto;
+}
+
+.display-item{
+  display: flex;
+  justify-content: space-between;
+  padding: 8px;
+  border-bottom: 1px solid #ddd;
+}
+
+.display-item:last-child {
+  border-bottom: none;
+}
+
+.item-key {
+  font-weight: bold;
+  color: #333;
+}
+
+.item-value {
+  color: #555;
+}
+</style>
